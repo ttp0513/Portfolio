3f72dc756d59f98f7ebbff341786bfce3057e93a
Trong Phan <phan0145@Trongs-MacBook-Pro.local>
2025-12-25 21:44:56 -0500

    Add path shortcut

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from "@tailwindcss/vite"; 
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()], // Add react, tailwind to use them in the profject
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"), // Creates a path alias so that @ always points to project’s src folder.
-    },
-  },
-});
-
-
+  plugins: [react()],
+})
